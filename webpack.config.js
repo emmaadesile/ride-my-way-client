993d33460e3d011f4e23f1ca64d6c98d2e876be0
Emmanuel Adesile <emma2adesile@gmail.com>
2018-08-22 11:29:41 +0100

    ft(signup): user can signup on the app
    - setup react and react-dom
    - setup webpack and devserver for live reload
    - add .babelrc file
    
    [(Delivers) #159946779]

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+
+const htmlplugin = new HTMLWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+})
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [htmlplugin]
+}
