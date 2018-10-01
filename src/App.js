14f949e11060edd47c515fc63eaea12fb0da7a39
Emmanuel Adesile <emma2adesile@gmail.com>
2018-10-01 09:05:52 +0100

    setup app configuration

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Homepage from './components/Homapage/Homepage';
+
+class App extends Component {
+  render() {
+    return (
+      <Homepage />
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from '@reach/router';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <ul>
+        <li><Link></Link></li>
+      </ul>
+    )
+  }
+}
+
+export default Navbar;
