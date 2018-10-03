d622679810ebedf74ac9da6961728816648c0ef7
Emmanuel Adesile <emma2adesile@gmail.com>
2018-10-03 18:10:15 +0100

    fix failing travis build

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { create } from 'react-test-renderer';
-import Adapter from 'enzyme-adapter-react-16';
 import App from '../App';
 
 
-describe("Tests for App component", () => {  
+describe("Tests for App component", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<App />, div);
