6d68fd934b8198372545c69f88c4cd4cc030a174
Emmanuel Adesile <emma2adesile@gmail.com>
2018-10-31 20:45:39 +0100

    nitpicks(fix test): fix failing tests for App.js
    [Delivers import { shallow } from "enzyme";]

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 import App from "../App";
 
-describe("Tests for App component", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe("App Container", () => {
+  test("renders the App without crashing", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
   });
 });
